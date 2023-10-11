c179c0a0af8f2b5fef3b7cb1b7af0e7cbf440238
Mirek Kratochvil <exa.exa@gmail.com>
2023-10-11 15:47:19 +0200

    run the generic type tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,8 @@
 
-import SBMLFBCModels
+import AbstractFBCModels as A
+import SBMLFBCModels as S
 using Test
 
-@testset "SBMLFBCModels tests" begin end
+@testset "SBMLFBCModels tests" begin
+    A.run_fbcmodel_type_tests(S.SBMLModel)
+end
