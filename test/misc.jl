82a29fa8e3318352716473974c5e7b3969577278
exaexa <exaexa@users.noreply.github.com>
2024-06-10 12:20:49 +0000

    automatic formatting
    triggered by @exaexa on PR #3

--- test/misc.jl
+++ test/misc.jl
@@ -1,8 +1,6 @@
 
 @testset "miscellaneous functions and corner cases" begin
-    @test_throws DomainError S.sbml_export_sbo(
-        A.Annotations("sbo" => ["123", "321"]),
-    )
+    @test_throws DomainError S.sbml_export_sbo(A.Annotations("sbo" => ["123", "321"]))
 
     @test S.sortunique([3, 2, 1, 2, 3, 2, 1, 2, 3]) == [1, 2, 3]
 end
